8704236fc2f7a4f72acf212fa17b058f733976d1
Suvam Singh <singhsupriya711@gmail.com>
2025-03-24 21:12:03 +0530

    REST API CRUD Completed

--- 07-spring-boot-rest-crud/40-spring-boot-rest-crud-employee -with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ 07-spring-boot-rest-crud/40-spring-boot-rest-crud-employee -with-spring-data-rest/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -3,7 +3,9 @@ package com.luv2code.springboot.cruddemo.dao;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository <Employee,Integer>{
     //that's is no need to write any code
     // no need of implementation class
